e7973a60fe411e341314eec6467696d35e387994
noorio-rocket <noorio@aucegypt.edu>
2020-07-21 00:26:05 +0200

    Challange Solved!

--- Dicee Challenge/index.js
+++ Dicee Challenge/index.js
@@ -1,2 +1,19 @@
 var randomNumber1 = Math.floor(Math.random()*6+1)
-console.log(randomNumber1)
+var randomImg1 = 'images/dice'+randomNumber1+'.png'
+var randomNumber2 = Math.floor(Math.random()*6+1)
+var randomImg2 = 'images/dice'+randomNumber2+'.png'
+document.querySelector('.img1').setAttribute('src',randomImg1)
+document.querySelector('.img2').setAttribute('src',randomImg2)
+
+if(randomNumber1 > randomNumber2)
+{
+    document.querySelector('h1').innerHTML = "Player 1 wins!";
+}
+else if (randomNumber2 > randomNumber1)
+{
+    document.querySelector('h1').innerHTML = "Player 2 wins!";
+}
+else
+{
+    document.querySelector('h1').innerHTML = "It's a Draw";
+}
